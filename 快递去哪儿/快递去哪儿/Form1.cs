61e55bee59480a4162d40fef444a74eb1faf7670
Sharuru <mave@foxmail.com>
2015-02-11 19:04:11 +0800

    Deserialize json

--- 快递去哪儿/快递去哪儿/Form1.cs
+++ 快递去哪儿/快递去哪儿/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.IO;
@@ -8,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Net;
+using System.Windows.Forms.VisualStyles;
+using Newtonsoft.Json;
 
 namespace 快递去哪儿
 {
@@ -47,7 +50,7 @@ namespace 快递去哪儿
             companyDict.Add("EMS-EMS", "ems");
             companyDict.Add("SFSY-顺丰速运", "shunfeng");
             companyDict.Add("STKD-申通快递", "shentong");
-            companyDict.Add("YDKD-韵达快递,","yunda");
+            companyDict.Add("YDKD-韵达快递","yunda");
             companyDict.Add("YTSD-圆通速递","yuantong");
             companyDict.Add("ZTKD-中通快递","zhongtong");
             comboBoxSingleCompany.DataSource = new BindingSource(companyDict, null);
@@ -62,13 +65,11 @@ namespace 快递去哪儿
             try
             {
                 HttpWebResponse HttpWResp = (HttpWebResponse) HttpWReq.GetResponse();
-                MessageBox.Show(HttpWResp.StatusDescription+Company+ID);
-                Stream abc = HttpWResp.GetResponseStream();
-                StreamReader aaa = new StreamReader(abc);
-                string html = aaa.ReadToEnd();
-                MessageBox.Show(html);
+                Stream rawStream = HttpWResp.GetResponseStream();
+                StreamReader r = new StreamReader(rawStream);
+                string rawJson = r.ReadToEnd();
                 HttpWResp.Close();
-
+                DeserializeJson(rawJson);
             }
             catch (Exception ex)
             {
@@ -77,5 +78,38 @@ namespace 快递去哪儿
             
         }
 
+        private void DeserializeJson(string rawJson)
+        {
+            var json = JsonConvert.DeserializeObject<Json>(rawJson);
+            MessageBox.Show("Starting...");
+            foreach (var data in json.data)
+            {
+                    MessageBox.Show(data.context.ToString());
+            }
+
+        }
+        public class Data
+        {
+            public string time { get; set; }
+            public string location { get; set; }
+            public string context { get; set; }
+            public string ftime { get; set; }
+        }
+
+
+        public class Json
+        {
+            public string message { get; set; }
+            public string nu { get; set; }
+            public string ischeck { get; set; }
+            public string com { get; set; }
+            public string updatetime { get; set; }
+            public string status { get; set; }
+            public string condition { get; set; }
+            public List<Data> data { get; set; }
+            public string state { get; set; }
+        }
+
     }
+
 }
